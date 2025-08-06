fc0e4a3ae2de6a2d545125d9e9927cf9672c4234
Bhavesh Mali <bhaveshmali2304@gmail.com>
2025-08-07 00:18:30 +0530

    setting up the project....

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import connectDB from "./db/db.js";
+import app from "./app.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path:"./.env"
+})
+
+connectDB()
+.then((result) => {
+    app.listen(process.env.PORT,() => {
+        console.log(`Server listening on ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Mongo connection error",error);    
+})
