fde612af805e117f0aa966aaa3f9a092c7e49869
Bhavesh Mali <bhaveshmali2304@gmail.com>
2025-08-14 00:37:13 +0530

    Configure Redis client with socket options for host and port

--- src/redis.js
+++ src/redis.js
@@ -1,6 +1,11 @@
 import {createClient} from "redis"
 
-const redis = createClient()
+const redis = createClient({
+    socket: {
+        host: "my-redis",
+        port: 6379
+    }
+});
 
 redis.on("error", (error) => {
     console.log("Redis connection error", error);
